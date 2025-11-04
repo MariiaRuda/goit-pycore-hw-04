0a86aa679aa3dbd1c444f1d8b109085125502e83
Mariia Ruda <masharuda290@gmail.com>
2025-11-05 00:07:55 +0200

    4_ex_added_ if for empty

--- ex_4/main.py
+++ ex_4/main.py
@@ -24,6 +24,8 @@ def show_phone(args, contacts):
     
 
 def show_all(contacts):
+    if not contacts:
+        return "📭 No contacts saved yet."
     return "усі збережені контакти з номерами телефонів:\n" + \
            "\n".join(f"{name}: {phone}" for name, phone in contacts.items())
 
@@ -32,32 +34,32 @@ def main(): # print/input тут
     contacts = {}
     print("Welcome to the assistant bot!")
     while True:
-            try:
-             user_input = input("Enter a command: ")
-             command, *args = parse_input(user_input)
+        try:
+            user_input = input("Enter a command: ")
+            command, *args = parse_input(user_input)
 
-             if command in ["close", "exit"]:
+            if command in ["close", "exit"]:
                  print("Good bye!")
                  break
-             elif command == "hello":
+            elif command == "hello":
                  print("How can I help you?")
-             elif command == "add":
+            elif command == "add":
                  print(add_contact(args, contacts))
-             elif command=="change":
+            elif command=="change":
                  print(change_contact(args, contacts))
-             elif command=="phone":
+            elif command=="phone":
                  print(show_phone(args, contacts))
-             elif command=="all":
+            elif command=="all":
                  print(show_all(contacts))
-             else:
+            else:
                  print("Invalid command.")
 
-            except ValueError:
-                 print("""⚠️ Invalid input format(expected: "[command] [username] [phone]"). Please check your command and try again.""")
-            except KeyError as e:
-                 print(f"⚠️ Contact not found: {e}")
-            except Exception as e:
-                 print(f"💥 Oops, something went wrong: {e}")
+        except ValueError:
+            print("""⚠️ Invalid input format(expected: "[command] [username] [phone]"). Please check your command and try again.""")
+        except KeyError as e:
+            print(f"⚠️ Contact not found: {e}")
+        except Exception as e:
+            print(f"💥 Oops, something went wrong: {e}")
 
 if __name__ == "__main__":
     main()
